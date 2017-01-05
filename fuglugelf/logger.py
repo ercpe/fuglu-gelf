357659fc51f4d0367e92170ab65dc7754ca385d0
Johann Schmitz <johann@j-schmitz.net>
2017-01-05 06:30:08 +0100

    Log subject

--- fuglugelf/logger.py
+++ fuglugelf/logger.py
@@ -86,6 +86,7 @@ class GELFLogger(AppenderPlugin):
                     _add_to_dict(result, v, path + [k])
         
         _add_to_dict(d, {'decision': decision}, [prefix])
+        _add_to_dict(d, {'subject', suspect.get_message_rep()['Subject'] or ""}, [prefix])
         _add_to_dict(d, suspect, [prefix])
 
         for i, rcvd in enumerate(reversed(self.info_from_rcvd(suspect))):
